689545feb8a36fc03a0f53635907cc431f640347
Tamojit Ghosh <72553595+tamojitghosh@users.noreply.github.com>
2022-04-18 15:30:45 +0530

    Update vissim_output.py

--- vissim_output.py
+++ vissim_output.py
@@ -1,12 +1,14 @@
 import pandas as pd
-with open("Simple_Network_001.mer", 'r') as fp:
+filename = "Simple_Network_001.mer"
+
+with open(filename, 'r') as fp:
     lines = fp.readlines()  # reading the output file from vissim
-with open("Simple_Network_001.mer", 'w') as fp:
+with open(filename, 'w') as fp:
     for number, line in enumerate(lines):
         if number not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
             fp.write(line)  # deleting the initial unwanted lines
 
-df = pd.read_table("Simple_Network_001.mer", sep=';')  # delimiting the dataframe by semicolon
+df = pd.read_table(filename, sep=';')  # delimiting the dataframe by semicolon
 df.columns = df.columns.str.strip()  # removing the space between the columns
 df1 = df.sort_values(by=['VehNo'])  # sorting the dataframe by vehicle number
 df2 = df1.loc[:, ~df.columns.str.contains('^Unnamed')]  # removing columns with no data 'unnamed'
