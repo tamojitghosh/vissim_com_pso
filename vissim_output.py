5a7724cd0577c04fc9270f1b343561525b7ebcce
Tamojit Ghosh <72553595+tamojitghosh@users.noreply.github.com>
2022-04-18 14:26:01 +0530

    Update vissim_output.py

--- vissim_output.py
+++ vissim_output.py
@@ -1,15 +1,15 @@
 import pandas as pd
+with open("Simple_Network_001.mer", 'r') as fp:
+    lines = fp.readlines()  # reading the output file from vissim
+with open("Simple_Network_001.mer", 'w') as fp:
+    for number, line in enumerate(lines):
+        if number not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+            fp.write(line)  # deleting the initial unwanted lines
 
-# with open("Simple_Network_001.mer", 'r') as fp:
-#     lines = fp.readlines()
-# with open("Simple_Network_001.mer", 'w') as fp:
-#     for number, line in enumerate(lines):
-#         if number not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
-#             fp.write(line)
+df = pd.read_table("Simple_Network_001.mer", sep=';')  # delimiting the dataframe by semicolon
+df.columns = df.columns.str.strip()  # removing the space between the columns
+df1 = df.sort_values(by=['VehNo'])  # sorting the dataframe by vehicle number
+df2 = df1.loc[:, ~df.columns.str.contains('^Unnamed')]  # removing columns with no data 'unnamed'
+df3 = df2[df2['t(Exit)'] != -1.00]  # removing the rows with -1 value in t(Exit)
 
-df = pd.read_table("Simple_Network_001.mer", sep=';')
-
-df.columns = df.columns.str.strip()
-df1 = df.sort_values(by=['VehNo'])
-df2 = df1.loc[:, ~df.columns.str.contains('^Unnamed')]
-print(df2.to_string())
+print(df3.to_string())
